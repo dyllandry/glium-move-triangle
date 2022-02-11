46adabc2369d1cf38c5a8a2dbfa03c4b92ca04be
Dylan Landry <moo@mooville.local>
2022-02-11 08:46:33 -0500

    initial commit

---
+++ readme.md
@@ -0,0 +1,5 @@
+![a red triangle on the left inside a blue window](screenshot-1.png)
+
+![a red triangle in the middle inside a blue window](screenshot-2.png)
+
+![a red triangle on the right inside a blue window](screenshot-3.png)
---
+++ src/main.rs
@@ -0,0 +1,102 @@
+#[macro_use]
+extern crate glium;
+
+#[derive(Copy, Clone)]
+struct Vertex {
+    position: [f32; 2],
+}
+
+implement_vertex!(Vertex, position);
+
+fn main() {
+    use glium::glutin;
+    use glium::Surface;
+
+    let event_loop = glutin::event_loop::EventLoop::new();
+    let wb = glutin::window::WindowBuilder::new();
+    let cb = glutin::ContextBuilder::new();
+    let display = glium::Display::new(wb, cb, &event_loop).unwrap();
+
+    let vertex_shader_src = r#"
+        #version 140
+
+        in vec2 position;
+
+        // Add t as a uniform to shader so the GPU can do the calculation instead of the CPU.
+        uniform float t;
+
+        void main() {
+            vec2 pos = position;
+            pos.x += t;
+            gl_Position = vec4(pos, 0.0, 1.0);
+        }
+    "#;
+
+    let fragment_shader_src = r#"
+        #version 140
+
+        out vec4 color;
+
+        void main() {
+            color = vec4(1.0, 0.0, 0.0, 1.0);
+        }
+    "#;
+
+    let program =
+        glium::Program::from_source(&display, &vertex_shader_src, &fragment_shader_src, None)
+            .unwrap();
+
+    let vertex1 = Vertex {
+        position: [-0.5, -0.5],
+    };
+    let vertex2 = Vertex {
+        position: [0.0, 0.5],
+    };
+    let vertex3 = Vertex {
+        position: [0.5, -0.25],
+    };
+    let shape = vec![vertex1, vertex2, vertex3];
+
+    let vertex_buffer = glium::VertexBuffer::new(&display, &shape).unwrap();
+
+    let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
+
+    let mut t: f32 = -0.5;
+
+    event_loop.run(move |ev, _, control_flow| {
+        let next_frame_time =
+            std::time::Instant::now() + std::time::Duration::from_nanos(16_666_667);
+        *control_flow = glutin::event_loop::ControlFlow::WaitUntil(next_frame_time);
+        match ev {
+            glutin::event::Event::WindowEvent { event, .. } => match event {
+                glutin::event::WindowEvent::CloseRequested => {
+                    *control_flow = glutin::event_loop::ControlFlow::Exit;
+                    return;
+                }
+                _ => return,
+            },
+            _ => (),
+        }
+
+        // Increase t
+        t += 0.0002;
+        if t > 0.5 {
+            t = -0.5;
+        }
+
+        let mut target = display.draw();
+        target.clear_color(0.0, 0.0, 1.0, 1.0);
+        target
+            .draw(
+                &vertex_buffer,
+                &indices,
+                &program,
+                // Pass t as a uniform to the shader so the GPU can do the calculation instead of
+                // the CPU.
+                &uniform! {t: t},
+                &Default::default(),
+            )
+            .unwrap();
+        target.finish().unwrap();
+    });
+}
